7376a68c796f51825a3b5df52611d94efd19d349
Ramon-darwin <RAMON.DARWIN@iisgalvanimi.edu.it>
2022-01-09 11:33:59 +0000

    punto due

---
+++ verifica-post-spammer/post-spammer/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { newArray } from '@angular/compiler/src/util';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'post-spammer';
+  articles2 = new Array<string>();
+}
